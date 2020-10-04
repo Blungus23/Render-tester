44da54cf373147a94e864ed56f6523e317985fd8
Noah Breedy <noahfamily373@gmail.com>
2020-10-04 12:42:26 -0700

    Added more people

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ app.set("view engine","ejs");
 app.get('/', function (req, res) {
   var person =  {name:'Mark',age:24}
   var person2 = {name:'Joe',age:45}
-  var persons = [person,person2]; 
+  var person3 = {name:'Rob',age:78}
+  var person4 = {name:'Mary',age:25}
+  var persons = [person,person2,person3,person4]; 
   res.render('WoW',{persons});
 });
 
