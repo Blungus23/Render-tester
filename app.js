0417cdd15afab96408142a2083e84bd55999ea27
Noah Breedy <noahfamily373@gmail.com>
2020-10-04 12:12:56 -0700

    Heroku Help

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.get('/', function (req, res) {
   var person2 = {name:'Joe',age:45}
   var persons = [person,person2]; 
   res.render('WoW',{persons});
+
+  
 });
 
 var server = app.listen(5000, function () {
