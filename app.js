885ea31d66d2423c8e9959e731aa903a46109378
Noah Breedy <noahfamily373@gmail.com>
2020-10-04 12:18:20 -0700

    B

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ app.get('/', function (req, res) {
   var person2 = {name:'Joe',age:45}
   var persons = [person,person2]; 
   res.render('WoW',{persons});
-
-  
 });
 
 var server = app.listen(5000, function () {
